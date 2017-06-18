3d32d8ef6e09320880da066765c1412ef9e2d708
carbonnolio <ynblpek83@gmail.com>
2017-06-18 09:59:58 -0400

    babel + webpack

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,10 +1,17 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
-var port = 3200;
+const port = 3200;
+const server = express();
+const compiler = webpack(config);
 
-var server = express();
+server.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 server.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
