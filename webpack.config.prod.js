53a281fb2eead09f286738dddd9873bd7e8aaa5c
carbonnolio <ynblpek83@gmail.com>
2017-06-24 10:16:24 -0400

    html templating

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -46,7 +46,8 @@ export default {
         minifyCSS: true,
         minifyURLs: true
       },
-      inject: true
+      inject: true,
+      someToken: 'Hello World'
     }),
 
     // Eliminate dup packages
